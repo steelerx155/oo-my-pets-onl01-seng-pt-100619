664d397556325282bca3ab9db86d5e27ee0f4dc7
Randy Steele <steelerx155@gmail.com>
2019-11-10 01:06:15 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,7 @@
 class Cat
   # code goes here
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
+require 'pry'
 class Owner
-  # code goes here
-end
+  
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @species = 'human'
+    @@all << self
+  end  
+  
+  def self.all
+    @@all 
+  end
+  
+  def say_species
+    "I am a #{species}."
+  end
+  
+ end 

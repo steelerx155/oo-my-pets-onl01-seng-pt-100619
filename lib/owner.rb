cdcafd71f17b5bd29eecd30836338eb437086267
Randy Steele <steelerx155@gmail.com>
2019-11-11 00:59:59 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Owner
 
-  attr_reader :name, :species, :cat
+  attr_reader :name, :species, :cat 
   
   @@all = []
   @@count = 0
@@ -38,6 +38,46 @@ class Owner
   Dog.all.select {|dog| dog.owner == self }
   end
   
-  def buy_cat
-    Cat.new @@all 
-end
+  def buy_cat(name)
+  # binding.pry  
+   cat = Cat.new(name, self)
+   cat
+  end
+    
+  def buy_dog(name)
+    dog = Dog.new(name, self)
+    dog
+  end
+ 
+  def walk_dogs
+    # binding.pry
+    self.dogs
+    Dog.all.select {|dog| dog.mood = 'happy'}
+  end
+  
+  def feed_cats
+    self.cats
+    Cat.all.select {|cat| cat.mood = 'happy'}
+  end  
+  
+  def sell_pets
+    self.cats
+    Cat.all.select {|cat| cat.mood = 'nervous'}
+    self.dogs
+    Dog.all.select {|dog| dog.mood = 'nervous'}
+    self.cats
+    Cat.all.select {|cat| cat.owner = nil }
+    self.dogs
+    Dog.all.select {|dog| dog.owner = nil }
+   end  
+   
+   def list_pets
+     self.cats.count
+     "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+    # self.dogs.count
+   end
+end 
+
+
+
+    
